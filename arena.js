b33b118e253184c5fe92c54f4a681b162a4c3275
Lukacs T. Berki <lberki@lberki.net>
2022-04-15 05:57:08 +0200

    Implement rudimentary freelist

--- arena.js
+++ arena.js
@@ -2,9 +2,12 @@
 
 // TODO:
 // - Figure out why # private members don't work
-// - Implement free() and a rudimentary freelist
 
-const HEADER_SIZE = 16;
+const util = require("util");
+const debuglog = util.debuglog("arena");
+
+const ARENA_HEADER_SIZE = 16;
+const BLOCK_HEADER_SIZE = 4;
 
 class Ptr {
     constructor(arena, base, size) {
@@ -21,48 +24,95 @@ class Ptr {
     
     get32(i) {
 	this._boundsCheck(i*4);
-	return this._arena.uint32[this._base / 4 + 1 + i];
+	return this._arena.uint32[(this._base + BLOCK_HEADER_SIZE) / 4 + i];
     }
 
     set32(i, x) {
 	this._boundsCheck(i*4);
-	this._arena.uint32[this._base / 4 + 1 + i] = x;
+	this._arena.uint32[(this._base + BLOCK_HEADER_SIZE) / 4 + i] = x;
     }
 	
 }
 
 class Arena {
     constructor(size) {
-	if (size % 16 != 0) {
+	if (size % 16 !== 0) {
 	    throw new Error("size must be a multiple of 16");
 	}
 	
-	this.bytes = new SharedArrayBuffer(HEADER_SIZE + size);
+	this.bytes = new SharedArrayBuffer(ARENA_HEADER_SIZE + size);
 	this.uint32 = new Uint32Array(this.bytes);
 	this.size = size;
-	this.freeStart = HEADER_SIZE;
-	this.freeEnd = HEADER_SIZE + size;
+	this.freeStart = ARENA_HEADER_SIZE;
+	this.freeEnd = ARENA_HEADER_SIZE + size;
+
+	this.uint32[0] = 0xd1ce4011;  // Magic
+	this.uint32[1] = 0;           // Start of freelist
+
+	debuglog("created arena(size=%d)", size);
+    }
+
+    _fromFreeList(size) {
+	let prev = 4;  // Address of start of freelist
+
+	while (true) {
+	    let next = this.uint32[prev / 4];
+	    if (next === 0) {
+		return null;  // end of freelist, nothing found
+	    }
+
+	    let nextSize = this.uint32[next / 4];
+	    if (nextSize === size) {
+		// Perfect match, remove from freelist and return
+		this.uint32[prev / 4] = this.uint32[(next + 4) / 4];
+		return next;
+	    } else if (nextSize >= size + BLOCK_HEADER_SIZE + 4) {
+		// Larger block, cut it in half
+		let secondHalf = next + size + BLOCK_HEADER_SIZE;
+		this.uint32[secondHalf / 4] = nextSize - size - BLOCK_HEADER_SIZE;
+
+		// Replace next in freelist with secondHalf
+		this.uint32[(secondHalf + 4) / 4] = this.uint32[(next + 4) / 4];
+		this.uint32[prev / 4] = secondHalf;
+
+		return next;
+	    } else {
+		prev = next + 4;  // Freelist is the first uint32 in the block
+	    }
+	}
     }
 
     alloc(size) {
-	if (size <= 0) {
+	if (size <= 0 || (size % 4) !== 0) {
 	    throw new Error("invalid size");
+	}
+
+	let fromFreeList = this._fromFreeList(size);
+	if (fromFreeList !== null) {
+	    this.uint32[fromFreeList / 4] = size;
+	    debuglog("allocated %d bytes @ %d from freelist", size, fromFreeList);
+	    return new Ptr(this, fromFreeList, size);
 	}
 	
 	let base = this.freeStart;
-	if (base + size > this.freeEnd) {
+	if (base + size + BLOCK_HEADER_SIZE > this.freeEnd) {
 	    throw new Error("out of memory");
 	}
 
-	this.freeStart += size;
+	this.freeStart += size + BLOCK_HEADER_SIZE;
 	this.uint32[base / 4] = size;
+
+	debuglog("allocated %d bytes @ %d by expansion", size, base);
 	return new Ptr(this, base, size);
     }
 
     free(ptr) {
+	debuglog("freeing %d bytes @ %d", this.uint32[ptr._base / 4], ptr._base);
+
+	let oldFreelistHead = this.uint32[1];
+	ptr.set32(0, oldFreelistHead);
+	this.uint32[1] = ptr._base;
     }
 }
-
-let wtf = new Arena(16);
 
 exports.Arena = Arena;
--- spec/arena_spec.js
+++ spec/arena_spec.js
@@ -9,9 +9,44 @@ describe("arena", () => {
 
     it("can allocate", () => {
 	let cut = new arena.Arena(32);
-	let ptr = cut.alloc(32);
+	let ptr = cut.alloc(28);
 	ptr.set32(0, 1);
 	expect(ptr.get32(0)).toBe(1);
+    });
+
+    it("can reallocate freed block", () => {
+	let cut = new arena.Arena(32);
+	let ptr = cut.alloc(28);
+	ptr.set32(2, 1);
+	cut.free(ptr);
+	ptr = cut.alloc(28);
+	expect(ptr.get32(2)).toBe(1);
+    });
+
+    it("can halve freed block", () => {
+	let cut = new arena.Arena(32);
+	let ptr = cut.alloc(28);
+	cut.free(ptr);
+	let ptr1 = cut.alloc(12);
+	let ptr2 = cut.alloc(12);
+    });
+
+    it("can skip block too small", () => {
+	let cut = new arena.Arena(48);
+	let small = cut.alloc(8);
+	let large = cut.alloc(16);
+	small.set32(1, 3);
+	large.set32(1, 4);
+
+	cut.free(large);
+	cut.free(small);
+
+	large = cut.alloc(16);
+	small = cut.alloc(8);
+
+	// Check if we got back the same blocks
+	expect(large.get32(1)).toBe(4);
+	expect(small.get32(1)).toBe(3);
     });
 
     it("can throw OOM", () => {
