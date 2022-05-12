00766af0bb92d0ff83040ac7ed9af98f88af88ce
Lukacs T. Berki <lberki@lberki.net>
2022-05-12 21:54:47 +0200

    More skeleton code for Array.
    That's all I can do today (or probably this week)

--- array.js
+++ array.js
@@ -50,11 +50,11 @@ function handlerSet(target, property, value, receiver) {
 }
 
 function handlerDeleteProperty(target, property) {
-    return target._deleteProperty(property);
+    throw new Error("not implemented");
 }
 
 function handlerHas(target, property) {
-    return target._has(property);
+    throw new Error("not implemented");
 }
 
 function handlerOwnKeys(target) {
@@ -101,6 +101,74 @@ class Array extends localobject.LocalObject {
 
     _free() {
 	super._free();
+    }
+
+    _impl_at(i) {
+	throw new Error("at() not implemented");
+    }
+
+    _impl_push(...args) {
+	throw new Error("push() not implemented");
+    }
+
+    _impl_pop() {
+	throw new Error("pop() not implemented");
+    }
+
+    _impl_unshift() {
+	throw new Error("unshift() not implemented");
+    }
+
+    _impl_shift() {
+	throw new Error("shift() not implemented");
+    }
+
+    _impl_concat() {
+	throw new Error("concat() not implemented");
+    }
+
+    _impl_slice() {
+	throw new Error("slice() not implemented");
+    }
+
+    _impl_splice() {
+	throw new Error("splice() not implemented");
+    }
+
+    _impl_values() {
+	throw new Error("values() not implemented");
+    }
+
+    _get(property) {
+	let idx = parseInt(property);
+	if (!isNaN(idx)) {
+	    // We have successfully converted an integer to string and back again, but let's at
+	    // least conform to the spec, if hilariously slowly
+	    console.log(idx);
+	    return this._impl_at(idx);
+	}
+
+	if (idx === "length") {
+	    throw new Error("length not implemented");
+	}
+
+	let implName = "_impl_" + property;
+	if (implName in this) {
+	    return this[implName].bind(this);
+	}
+
+	return undefined;
+    }
+
+    _set(property, value) {
+	let idx = parseInt(property);
+	if (idx !== NaN) {
+	    // Set numeric index.
+	}
+
+	// TODO: according to spec, we should be able to set arbitrary keys even though it requires
+	// an auxiliary Dictionary instance.
+	throw new Error("not implemented");
     }
 }
 
